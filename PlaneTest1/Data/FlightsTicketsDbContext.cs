d13b56303e7c1ee0592b53c4f8728179fbe62dd3
bgmastery <60539405+bgmastery@users.noreply.github.com>
2022-03-26 14:34:55 +0200

    new site with login and db's

---
+++ PlaneTest1/Data/FlightsTicketsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlaneTest1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlaneTest1.Data
+{
+    public class FlightsTicketsDbContext : DbContext
+    {
+        public DbSet<Flights> Flights { get; set; }
+        public DbSet<Tickets> Tickets { get; set; }
+        public FlightsTicketsDbContext(DbContextOptions<FlightsTicketsDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
